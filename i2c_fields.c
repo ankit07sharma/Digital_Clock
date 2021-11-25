824536a680a42548a3b41d23ee5cec466b64b299
Ankit sharma <82817419+ankit07sharma@users.noreply.github.com>
2021-11-25 12:41:38 +0530

    Update i2c_fields.c

--- i2c_fields.c
+++ i2c_fields.c
@@ -1,7 +1,13 @@
+//This function consist of all I2C fields which will be used in creating read and write frame for I2C, because we don't have a inbuilt pheripheral I2C hardware in
+//8051
+
 #include<reg51.h>
 #include<intrins.h>
+
 sbit SDA=P1^0;
 sbit SCL=P1^1;
+
+//This function will create a start condition on I2C bus
 void i2c_start()
 {
 	SCL=1;          //idle condition  optional
@@ -12,6 +18,8 @@ void i2c_start()
 	SDA=1;         //SDA will become high to low when SCL is high
 	SDA=0;
 }
+
+//This function will create a stop condition on I2C bus
 void i2c_stop()
 	
 {
@@ -20,6 +28,8 @@ void i2c_stop()
 	SCL=1;         //SDA become low to high when SCL is high
 	SDA=1;
 }
+
+//This function will allow master to write one byte data to I2C bus
 void i2c_write(unsigned char d)
 {
 char i;
@@ -30,6 +40,8 @@ char i;
 		SCL=1;
 	}
 }
+
+//This function will allow master to read one byte data from I2C bus
 unsigned char i2c_read()
 {
 	char i;
@@ -43,6 +55,8 @@ unsigned char i2c_read()
 	}
 	return temp;
 }
+
+//This function will receive acknowledgement from slave after receving one byte
 bit i2c_ack()
 {
 	SCL=0;
@@ -63,32 +77,14 @@ bit i2c_ack()
 	}
 	
 }
+
+//This function will allow master to give no acknowledgement after master will read one byte from I2C bus
+
 void i2c_noack()
 {
 	SCL=0;
 	SDA=1;
 	SCL=1;
 }
-//bit i2c_ack1()
-//{ SDA=0;
-//  SCL=0;
-//	_nop_();     //delay 1 microsecond
-//	_nop_();
-//	_nop_();
-//	_nop_();
-//    	//compalsary
-//	SCL=1;
-
-//	if(SDA==0)
-//	{
-//		SCL=0;
-//	return 0;
-//	}
-//	else{
-//		SCL=0;
-//		return 1;
-//	}
-//}
-//	
 
 	
